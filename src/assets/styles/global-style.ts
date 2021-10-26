5e4f89294f0ed5a14980741fe46ba92e5f48e60c
강석현 <tjrgus0713@gmail.com>
2021-10-26 23:20:26 +0900

    [추가] global-style 변경

--- src/assets/styles/global-style.ts
+++ src/assets/styles/global-style.ts
@@ -2,14 +2,40 @@ import { createGlobalStyle } from "styled-components";
 import normalize from "normalize-package-data";
 
 const GlobalStyle = createGlobalStyle`
-    html,
     body {
-        overflow: hidden;
-    }
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+* {
+  margin: 0;
+  font-family: "Noto Sans CJK KR";
+  font-size: 16px;
+  font-weight: normal;
+}
+input {
+  padding: 0;
+  outline: none;
+}
+a {
+  text-decoration: none;
+  color: black;
+}
+p {
+  margin: 0%;
+}
+ul,
+li {
+  padding: 0;
+  list-style: none;
+}
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+    monospace;
+}
 
-    * {
-        box-sizing: border-box;
-        margin: 0;
-    }
 `
 export default GlobalStyle;
