921ef44f47fd2a3afe1ce6688b74fd181f3c6200
강석현 <tjrgus0713@gmail.com>
2021-10-27 20:25:51 +0900

    [추가] 메인 페이지에 상품리스트 박스 추가

--- src/Pages/MainPage/MainPage.tsx
+++ src/Pages/MainPage/MainPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import * as S from './styles'
 import Banner from '../../Components/Banner/Banner';
+import GoodsListBox from '../../Components/GoodsListBox/GoodsListBox';
 const MainPage:React.FC = () => {
     return(
         <S.MainPageWrapper>
             <Banner/>
+            <GoodsListBox/>
         </S.MainPageWrapper>
     )
 }
