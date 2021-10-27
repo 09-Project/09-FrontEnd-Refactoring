077c8c30b4ab294bd8b56f469415dddc167e23a6
강석현 <tjrgus0713@gmail.com>
2021-10-27 20:25:00 +0900

    [추가] 각각의 상품을 나타낼 박스 추가

---
+++ src/Components/EachGoodsBox/EachGoodsBox.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as S from './styles';
+
+interface PropsType {
+    item : any;
+}
+const EachGoodsBox = (props:PropsType) => {
+    return(
+        <S.GoodsBoxWrapper>
+            <img src="" alt="" />
+        </S.GoodsBoxWrapper>
+    )
+}
+export default EachGoodsBox;
---
+++ src/Components/EachGoodsBox/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const GoodsBoxWrapper = styled.div`
+    width: 220px;
+    height: 100px;
+    padding: 0 43px 40px 0;
+`
