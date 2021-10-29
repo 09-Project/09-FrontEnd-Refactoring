5bd854641a2022e842791c339a88738f4dc9d9d8
강석현 <tjrgus0713@gmail.com>
2021-10-30 00:00:52 +0900

    [추가] react-router 설치 및 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import MainPage from "./Pages/MainPage/MainPage";
+import Header from "./Components/Header/Header";
+import { BrowserRouter,Switch,Route } from "react-router-dom";
+import Upload from "./Pages/upload/upload";
 const App:React.FC = () => {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+        <Header/>
+      <Switch>
+        <Route exact path="/" component={MainPage}/>
+        <Route path="/upload" component={Upload}/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
