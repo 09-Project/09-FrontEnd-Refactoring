87147626e76ddcfab378234b5e89b9dad86b0923
강석현 <tjrgus0713@gmail.com>
2021-11-01 11:03:02 +0900

    [추가] 로고 클릭 시 메인페이지로 이동할 수 있도록 Link 추가

--- src/Components/Header/HeaderBottom/SearchBox.tsx
+++ src/Components/Header/HeaderBottom/SearchBox.tsx
@@ -4,14 +4,17 @@ import Symbol from '../../../assets/images/Symbol.png'
 import Logo09 from '../../../assets/images/logo_09.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom';
 
 const SearchBox = () => {
     return(
         <S.HeaderLogoNSearch>
-            <S.LogoNSymbol>
-                <S.Logo img={Symbol} margin={11}/>
-                <S.Logo isMain={true} img={Logo09}/>
-            </S.LogoNSymbol>
+            <Link to ="/">
+                <S.LogoNSymbol>
+                    <S.Logo img={Symbol} margin={11}/>
+                    <S.Logo isMain={true} img={Logo09}/>
+                </S.LogoNSymbol>
+            </Link>
             <S.SearchBox>
                 <input type="text" placeholder="상품명을 입력하세요." />
                 <FontAwesomeIcon style={{marginLeft:'10px'}} icon={faSearch}/>
