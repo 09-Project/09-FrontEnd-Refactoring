73af1e280d660114c65eca904b42c79039da2297
강석현 <tjrgus0713@gmail.com>
2021-11-01 11:01:13 +0900

    [추가] 상품 박스에 style적용

--- src/Components/EachGoodsBox/EachGoodsBox.tsx
+++ src/Components/EachGoodsBox/EachGoodsBox.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import * as S from './styles';
-
+import Location from '../../assets/images/location.gif'
 interface PropsType {
     item : any;
 }
 const EachGoodsBox = (props:PropsType) => {
     return(
         <S.GoodsBoxWrapper>
-            <img src="" alt="" />
+            <S.GoodsLink>
+                <S.GoodsImgDiv>
+                    <S.GoodsImg img={props.item.image} />
+                </S.GoodsImgDiv>
+                <S.GoodsInfo>
+                    <p>{props.item.title}</p>
+                    <div>
+                        <div>
+                            {props.item.purpose === "DONATION" ? <S.Donation>무료나눔</S.Donation> : <div><S.Price>{props.item.price}</S.Price><S.Won>원</S.Won></div>}
+                        </div>
+                    </div>
+                    <S.PostType>
+                        <p>{props.item.purpose === "DONATION" ? '기부' : '공동구매'}</p>
+                    </S.PostType>
+                </S.GoodsInfo>
+            </S.GoodsLink>
+            <S.Location>
+                <i>
+                    <img src={Location} alt="" />
+                </i>
+                <div>{props.item.transaction_region}</div>
+            </S.Location>
         </S.GoodsBoxWrapper>
     )
 }
--- src/Components/EachGoodsBox/styles.ts
+++ src/Components/EachGoodsBox/styles.ts
@@ -1,7 +1,126 @@
 import styled from "styled-components";
+import Default from '../../assets/images/defaultProfile.svg'
+interface PorpsType {
+    img? : any
+}
 
 export const GoodsBoxWrapper = styled.div`
     width: 220px;
-    height: 100px;
-    padding: 0 43px 40px 0;
+    margin: 0 43px 40px 0;
+`
+export const GoodsLink = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    box-sizing: border-box;
+    background-color: white;
+`
+
+export const GoodsImgDiv = styled.div`
+    width: 100%;
+    height: 220px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #9a9a9a;
+    position: relative;
+`
+export const GoodsImg = styled.div`
+    width: 100%;
+    height: 100%;
+    color: white;
+    font-size: 30px;
+    font-family: "Noto Sans CJK KR";
+    font-weight: bold;
+    background-image: url(${(props:PorpsType)=>props.img || Default});
+    background-repeat: no-repeat;
+    background-size: cover;
+`
+export const GoodsInfo = styled.section`
+    width: 100%;
+    border-bottom: 1px solid #707070;
+    position: relative;
+    > p {
+        margin: 0;
+        padding: 11px 0 0 16px;
+        font-size: 18px;
+        font-family: "Noto Sans CJK KR";
+    }
+    >div{
+        padding: 4px 0 0 16px;
+        margin: 0;
+        display: flex;
+        font-family: "Noto Sans CJK KR";
+        padding-bottom: 12px;
+        >div{
+            display:flex;
+            > p {
+                font-weight: bold;
+                margin: 0;
+                font-size: 24px;
+                margin-right: 4px;
+            }
+            > div {
+                font-size: 16px;
+                margin-top: 11px;
+                display: flex;
+            }
+        }
+    }
+
+`
+export const PostType = styled.section`
+    position: absolute;
+    background-color: #4a55b5;
+    color: white;
+    border-radius: 5px;
+    display: flex;
+    width: 60px;
+    height: 30px;
+    align-items: center;
+    justify-content: center;
+    padding: 0 5px;
+    right: 8px;
+    bottom: 14px;
+    > p {
+        font-weight: bold;
+        font-family: "Noto Sans CJK KR";
+        display: flex;
+    }
+`
+export const Price = styled.p`
+    font-weight: bold;
+    margin: 0;
+    font-size: 24px;
+    margin-right: 4px;
+`
+export const Won = styled.p`
+    font-size: 16px;
+    margin-top: 11px;
+`
+export const Donation = styled.p`
+    font-size: 24px;
+    font-weight: bold;
+    display: flex;
+    color: black;
+`
+export const Location = styled.section`
+    display: flex;
+    align-items: center;
+    padding: 5px 0;
+    background-color: white;
+    > i {
+        width : 12px;
+        height:16px;
+        margin: 0 0 0 10px;
+        display: flex;
+        align-items: center;
+        >img{
+            width: 100%;
+            height: 100%;
+        }
+    }
+    >div {
+        margin: 0 0 0 8px;
+    }
 `
