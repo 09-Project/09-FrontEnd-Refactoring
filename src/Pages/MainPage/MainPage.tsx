76c3176b33de4de294bf41c9fda7c312dca7a128
강석현 <tjrgus0713@gmail.com>
2021-11-08 10:58:05 +0900

    [수정] banner의 props로 이미지와 크기를 넘기도록 수정

--- src/Pages/MainPage/MainPage.tsx
+++ src/Pages/MainPage/MainPage.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import * as S from './styles'
 import Banner from '../../Components/Banner/Banner';
 import GoodsListBox from '../../Components/GoodsListBox/GoodsListBox';
+import Banner_09 from '../../assets/images/banner_09.jpg'
 const MainPage:React.FC = () => {
     return(
         <S.MainPageWrapper>
-            <Banner/>
+            <Banner isLarge={true} img={Banner_09}/>
             <GoodsListBox/>
         </S.MainPageWrapper>
     )
