b758cce120fc7e19487bb8cba05409a7b163df89
강석현 <tjrgus0713@gmail.com>
2021-11-09 16:10:43 +0900

    [추가] 게시물 박스 클릭 시 해당 게시글의 상세페이지로 이동

--- src/Components/EachGoodsBox/EachGoodsBox.tsx
+++ src/Components/EachGoodsBox/EachGoodsBox.tsx
@@ -1,28 +1,31 @@
 import React from 'react';
 import * as S from './styles';
 import Location from '../../assets/images/location.gif'
+import { Link } from 'react-router-dom';
 interface PropsType {
     item : any;
 }
 const EachGoodsBox = (props:PropsType) => {
     return(
         <S.GoodsBoxWrapper>
-            <S.GoodsLink>
-                <S.GoodsImgDiv>
-                    <S.GoodsImg img={props.item.image} />
-                </S.GoodsImgDiv>
-                <S.GoodsInfo>
-                    <p>{props.item.title}</p>
-                    <div>
+            <Link to={'/product?post_id='+props.item.id}>
+                <S.GoodsLink>
+                    <S.GoodsImgDiv>
+                        <S.GoodsImg img={props.item.image} />
+                    </S.GoodsImgDiv>
+                    <S.GoodsInfo>
+                        <p>{props.item.title}</p>
                         <div>
-                            {props.item.purpose === "DONATION" ? <S.Donation>무료나눔</S.Donation> : <div><S.Price>{props.item.price}</S.Price><S.Won>원</S.Won></div>}
+                            <div>
+                                {props.item.purpose === "DONATION" ? <S.Donation>무료나눔</S.Donation> : <div><S.Price>{props.item.price}</S.Price><S.Won>원</S.Won></div>}
+                            </div>
                         </div>
-                    </div>
-                    <S.PostType>
-                        <p>{props.item.purpose === "DONATION" ? '기부' : '공동구매'}</p>
-                    </S.PostType>
-                </S.GoodsInfo>
-            </S.GoodsLink>
+                        <S.PostType>
+                            <p>{props.item.purpose === "DONATION" ? '기부' : '공동구매'}</p>
+                        </S.PostType>
+                    </S.GoodsInfo>
+                </S.GoodsLink>
+            </Link>
             <S.Location>
                 <i>
                     <img src={Location} alt="" />
