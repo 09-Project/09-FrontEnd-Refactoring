29686cf3b195c05f9b033484b642869c4ce5ce0c
강석현 <tjrgus0713@gmail.com>
2021-11-11 20:04:36 +0900

    [추가] 리덕스덕스 추가

--- src/modules/redux/action/auth/index.ts
+++ src/modules/redux/action/auth/index.ts
@@ -3,21 +3,29 @@ import { ACCESS_TOKEN,IS_LOGIN } from "./interface";
 export const setLogin = () => ({
     type : IS_LOGIN
 })
+export const setAccessToken = (token:string) => ({
+    type : ACCESS_TOKEN,
+    payload : token
+})
 
-type authActionType =  ReturnType<typeof setLogin>
+type authActionType =  ReturnType<typeof setLogin> | ReturnType<typeof setAccessToken>
 
 type isLoginState = {
     isLogin : boolean;
+    accessToken : string;
 }
 
 const initState:isLoginState = {
-    isLogin : false
+    isLogin : localStorage.getItem("access_token") ? true : false,
+    accessToken : ''
 }
 
 function setIsLogin(state : isLoginState = initState,action : authActionType):isLoginState{
     switch (action.type){
         case IS_LOGIN:
-            return {isLogin : !state.isLogin}
+            return {...state,isLogin : !state.isLogin}
+        case ACCESS_TOKEN:
+            return {...state,accessToken : action.payload}
         default:
             return state
     }
