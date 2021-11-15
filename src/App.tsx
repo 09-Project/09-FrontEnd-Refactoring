72ca646ddcd5b5a8822e2b6d0ae6cc90cd54b474
강석현 <tjrgus0713@gmail.com>
2021-11-15 10:59:30 +0900

    [추가] 디테일 및 라이브러리 추가

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,56 @@ import LoginModal from "./Components/LoginModal";
 import SignUp from './Pages/signup';
 import { useLocation } from 'react-router-dom';
 import GoodsDetail from './Pages/goodsDetail';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from './modules/redux';
+import axios from 'axios';
+import { API_HOST } from './constant/api';
+
 
 const App:React.FC = () => {
-  const [login,setLogin] = useState(false);
+  const dispatch = useDispatch();
+  useEffect(()=> {
+    console.log("access_token is changed")
+    const access_token = localStorage.getItem("access_token");
+    axios.get(API_HOST+'/member/information',{
+      headers:{Authorization : `Bearer ${access_token}`}
+    })
+    .then(()=>{
+      axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem("access_token")}`;
+    })
+    .catch(()=>{
+      localStorage.removeItem("access_token");
+    });
+  },[localStorage.getItem("access_token")])
+
+  console.log('기본 : '+axios.defaults.headers.common['Authorization'])
   const [signup,setSignup] = useState(false);
+  const Modalstatus = useSelector((state:RootState)=>state.setmodal.modal);
+
+  const axiosApiInstance = axios.create();
+    axiosApiInstance.interceptors.request.use(
+        async config => {
+          console.log('start')
+            const accessToken = localStorage.getItem("access_token")
+            config.headers = { 
+                'Authorization': `Bearer ${accessToken}`,
+                'Accept': 'application/json',
+            }
+            return config;
+        },
+        err => {
+            Promise.reject(err);
+        }
+    )
   return (
     <BrowserRouter>
-    {login ? 
-        <LoginModal setLogin={setLogin}/> : ''
+    {Modalstatus ? 
+      <LoginModal/>
+      : ''
     }
     {signup ? 
         ''
-    : <Header setLogin={setLogin}/>}
+    : <Header/>}
     <Switch>
         <Route exact path="/" component={MainPage}/>
         <Route path="/upload" component={Upload}/>
