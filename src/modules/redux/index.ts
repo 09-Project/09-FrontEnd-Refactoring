6489f25e8822de22b3d3325350077bdc53a5842a
강석현 <tjrgus0713@gmail.com>
2021-11-16 10:41:53 +0900

    [수정] 필요없는 리듀서 삭제

--- src/modules/redux/index.ts
+++ src/modules/redux/index.ts
@@ -2,15 +2,11 @@ import { combineReducers } from "redux";
 import setPage from "./action/signup";
 import setIsLogin from "./action/auth";
 import setModal from "./action/modal";
-import setMyInfoReducer from "./action/myinfo";
-import setUpload from "./action/upload";
 
 const rootReducer = combineReducers({
-    setpage : setPage,
-    setIsLogin : setIsLogin,
-    setmodal : setModal,
-    myinfo : setMyInfoReducer,
-    upload : setUpload
+    setpage: setPage,
+    setIsLogin: setIsLogin,
+    setmodal: setModal,
 });
 
 export default rootReducer
