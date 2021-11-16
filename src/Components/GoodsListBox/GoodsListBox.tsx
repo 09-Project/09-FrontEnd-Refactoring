fd0425cb81cbdcce963f2cebd6459cf1b5b05af7
강석현 <tjrgus0713@gmail.com>
2021-11-16 10:37:56 +0900

    [추가] 로그인 상태에 따라 게시글 불러오는 api 헤더에 토큰을 실도록 변경

--- src/Components/GoodsListBox/GoodsListBox.tsx
+++ src/Components/GoodsListBox/GoodsListBox.tsx
@@ -1,42 +1,66 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { API_HOST } from '../../constant/api';
+import { RootState } from '../../modules/redux';
 import EachGoodsBox from '../EachGoodsBox/EachGoodsBox';
 import * as S from './style';
+
+interface PageInfoType {
+    id: number;
+    title: string;
+    price?: number,
+    transaction_region: string
+    purpose: string
+    completed: string
+    created_date: string
+    updated_date: string
+    image: string
+    liked: boolean
+}
+
 const GoodsListBox = () => {
-    const [startPageIndex,setStartPageIndex] = useState(1);
-    const [selectedPage,setSelectedPage] = useState(1);
-    const [thisPageList,setThisPageList] = useState([]);
-    const onClickSelectBtn = (e:number) => {
+    const isLogin = useSelector((state: RootState) => state.setIsLogin.isLogin);
+    const [startPageIndex, setStartPageIndex] = useState(1);
+    const [selectedPage, setSelectedPage] = useState(1);
+    const [thisPageList, setThisPageList] = useState([]);
+    const onClickSelectBtn = (e: number) => {
         setSelectedPage(e);
     }
-    const onClickSetStartPage = (e:string) => {
-        if(e === 'preve') setStartPageIndex(Math.max(startPageIndex - 1, 1));
+    const onClickSetStartPage = (e: string) => {
+        if (e === 'preve') setStartPageIndex(Math.max(startPageIndex - 1, 1));
         else setStartPageIndex(startPageIndex + 1);
     }
-    useEffect(()=> {
-        axios.get(API_HOST+'/post').then(res=>setThisPageList(res.data.posts))
-    },[selectedPage])
-    return(
+    useEffect(() => {
+        const login = localStorage.getItem('access_token')
+        if (login) {
+            axios.get(API_HOST + '/post', {
+                headers: { Authorization: `Bearer ${login}` }
+            }).then(res => setThisPageList(res.data.posts))
+        } else {
+            axios.get(API_HOST + '/post').then(res => setThisPageList(res.data.posts))
+        }
+    }, [selectedPage, isLogin])
+    return (
         <S.GoodsListBoxWrapper>
             <S.Title>추천 상품</S.Title>
             <S.ListBox>
-                {thisPageList.map((item,index)=>{
-                    return <EachGoodsBox item={item}/>
+                {thisPageList.map((item, index) => {
+                    return <EachGoodsBox item={item} />
                 })}
             </S.ListBox>
             <S.ChangePageBtns>
-                <S.ArrowBtn onClick={()=>onClickSetStartPage('preve')}>{"<"}</S.ArrowBtn>
-                {Array(5).fill(void 0).map((item,index)=>{
+                <S.ArrowBtn onClick={() => onClickSetStartPage('preve')}>{"<"}</S.ArrowBtn>
+                {Array(5).fill(void 0).map((item, index) => {
                     const targetPage = startPageIndex + index;
-                    if(targetPage === selectedPage) return(
-                        <S.ChangePageBtn onClick={()=>onClickSelectBtn(targetPage)}>{targetPage}</S.ChangePageBtn>
+                    if (targetPage === selectedPage) return (
+                        <S.ChangePageBtn onClick={() => onClickSelectBtn(targetPage)}>{targetPage}</S.ChangePageBtn>
                     )
                     return (
-                        <S.SelectBtn onClick={()=>onClickSelectBtn(targetPage)}>{targetPage}</S.SelectBtn>
+                        <S.SelectBtn onClick={() => onClickSelectBtn(targetPage)}>{targetPage}</S.SelectBtn>
                     )
                 })}
-                <S.ArrowBtn onClick={()=>onClickSetStartPage('next')}>{">"}</S.ArrowBtn>
+                <S.ArrowBtn onClick={() => onClickSetStartPage('next')}>{">"}</S.ArrowBtn>
             </S.ChangePageBtns>
         </S.GoodsListBoxWrapper>
     )
