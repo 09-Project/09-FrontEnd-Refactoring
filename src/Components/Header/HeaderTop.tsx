c81b282dde5be729f46af4342edadf51e41db8d6
강석현 <tjrgus0713@gmail.com>
2021-11-16 10:38:28 +0900

    [수정] 바뀐 리덕스의 setLogin 함수에 맞게 수정

--- src/Components/Header/HeaderTop.tsx
+++ src/Components/Header/HeaderTop.tsx
@@ -3,8 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { RootState } from '../../modules/redux';
-import {setModalOn} from '../../modules/redux/action/modal'
-import {setLogin} from '../../modules/redux/action/auth'
+import { setModalOn } from '../../modules/redux/action/modal'
+import { setLogin } from '../../modules/redux/action/auth'
 
 const HeaderTopWrapper = styled.section`
     width: 1150px;
@@ -27,13 +27,14 @@ const LoginStatusDiv = styled.section`
 `
 
 const HeaderTop = () => {
-    const isLogin = useSelector((state:RootState)=>state.setIsLogin.isLogin);
+    const isLogin = useSelector((state: RootState) => state.setIsLogin.isLogin);
+    // const isLogin = localStorage.getItem('access_token');
     const dispatch = useDispatch()
     const ModalOn = () => dispatch(setModalOn())
-    const SetLogin = () => dispatch(setLogin())
-    return <HeaderTopWrapper>{isLogin ? <LoginStatusDiv><p onClick={()=>{
-        SetLogin();
+    const SetLogin = (status: boolean) => dispatch(setLogin(status))
+    return <HeaderTopWrapper>{isLogin ? <LoginStatusDiv><p onClick={() => {
+        SetLogin(false);
         localStorage.removeItem("access_token");
-    }}>로그아웃</p></LoginStatusDiv> : <LoginStatusDiv><p onClick={()=>ModalOn()}>로그인</p><div>/</div><Link to="/signup"><p>회원가입</p></Link></LoginStatusDiv>}</HeaderTopWrapper>
+    }}>로그아웃</p></LoginStatusDiv> : <LoginStatusDiv><p onClick={() => ModalOn()}>로그인</p><div>/</div><Link to="/signup"><p>회원가입</p></Link></LoginStatusDiv>}</HeaderTopWrapper>
 }
 export default HeaderTop;
