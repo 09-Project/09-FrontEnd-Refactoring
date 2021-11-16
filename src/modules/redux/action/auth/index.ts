54e7e8fc5f79e4d6fc1ea03fcbe10be98360719a
강석현 <tjrgus0713@gmail.com>
2021-11-16 10:39:12 +0900

    [수정] setLogin함수에 매개변수로 boolean값을 받도록 수정

--- src/modules/redux/action/auth/index.ts
+++ src/modules/redux/action/auth/index.ts
@@ -1,33 +1,34 @@
 import { AxiosResponse } from "axios";
-import { ACCESS_TOKEN,IS_LOGIN } from "./interface";
+import { ACCESS_TOKEN, IS_LOGIN } from "./interface";
 
-export const setLogin = () => ({
-    type : IS_LOGIN
+export const setLogin = (status: boolean) => ({
+    type: IS_LOGIN,
+    payload: status
 })
-export const setAccessToken = (token:string) => ({
-    type : ACCESS_TOKEN,
-    payload : token
+export const setAccessToken = (token: string) => ({
+    type: ACCESS_TOKEN,
+    payload: token
 })
 
-type authActionType =  ReturnType<typeof setLogin> | ReturnType<typeof setAccessToken> 
+type authActionType = ReturnType<typeof setLogin> | ReturnType<typeof setAccessToken>
 
 type isLoginState = {
-    isLogin : boolean;
-    accessToken : string;
-    
+    isLogin: boolean;
+    accessToken: string;
+
 }
 
-const initState:isLoginState = {
-    isLogin : localStorage.getItem("access_token") ? true : false,
-    accessToken : '',
+const initState: isLoginState = {
+    isLogin: localStorage.getItem("access_token") ? true : false,
+    accessToken: '',
 }
 
-function setIsLogin(state : isLoginState = initState,action : authActionType):isLoginState{
-    switch (action.type){
+function setIsLogin(state: isLoginState = initState, action: authActionType): isLoginState {
+    switch (action.type) {
         case IS_LOGIN:
-            return {...state,isLogin : !state.isLogin}
+            return { ...state, isLogin: action.payload }
         case ACCESS_TOKEN:
-            return {...state,accessToken : action.payload}
+            return { ...state, accessToken: action.payload }
         default:
             return state
     }
--- src/modules/redux/action/upload/interface/index.ts
+++
@@ -1,6 +0,0 @@
-export const UPLOAD_TYPE = 'upload/UPLOAD_TYPE' as const
-export const UPLOAD_IMG = 'upload/UPLOAD_IMG' as const
-export const TITLE = 'upload/TITLE' as const
-export const CONTENT = 'upload/CONTENT' as const
-export const PRICE = 'upload/PRICE' as const
-export const TRANSACTION_REGION = 'upload/TRANSACTIONREGION' as const
