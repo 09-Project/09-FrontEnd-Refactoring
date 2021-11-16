02c8d54363f2b1487e91ac692cdee4187d5c80c3
강석현 <tjrgus0713@gmail.com>
2021-11-16 10:42:05 +0900

    [수정] 디테일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React,{useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import MainPage from "./Pages/MainPage/MainPage";
 import Header from "./Components/Header/Header";
-import { BrowserRouter,Switch,Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Upload from "./Pages/upload";
 import Profile from "./Components/Porfile";
 import LoginModal from "./Components/LoginModal";
@@ -14,56 +14,56 @@ import axios from 'axios';
 import { API_HOST } from './constant/api';
 
 
-const App:React.FC = () => {
+const App: React.FC = () => {
   const dispatch = useDispatch();
-  useEffect(()=> {
+  useEffect(() => {
     console.log("access_token is changed")
     const access_token = localStorage.getItem("access_token");
-    axios.get(API_HOST+'/member/information',{
-      headers:{Authorization : `Bearer ${access_token}`}
+    axios.get(API_HOST + '/member/information', {
+      headers: { Authorization: `Bearer ${access_token}` }
     })
-    .then(()=>{
-      axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem("access_token")}`;
-    })
-    .catch(()=>{
-      localStorage.removeItem("access_token");
-    });
-  },[localStorage.getItem("access_token")])
+      .then(() => {
+        axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem("access_token")}`;
+      })
+      .catch(() => {
+        localStorage.removeItem("access_token");
+      });
+  }, [localStorage.getItem("access_token")])
 
-  console.log('기본 : '+axios.defaults.headers.common['Authorization'])
-  const [signup,setSignup] = useState(false);
-  const Modalstatus = useSelector((state:RootState)=>state.setmodal.modal);
+  // console.log('기본 : ' + axios.defaults.headers.common['Authorization'])
+  const [signup, setSignup] = useState(false);
+  const Modalstatus = useSelector((state: RootState) => state.setmodal.modal);
 
   const axiosApiInstance = axios.create();
-    axiosApiInstance.interceptors.request.use(
-        async config => {
-          console.log('start')
-            const accessToken = localStorage.getItem("access_token")
-            config.headers = { 
-                'Authorization': `Bearer ${accessToken}`,
-                'Accept': 'application/json',
-            }
-            return config;
-        },
-        err => {
-            Promise.reject(err);
-        }
-    )
+  axiosApiInstance.interceptors.request.use(
+    async config => {
+      console.log('start')
+      const accessToken = localStorage.getItem("access_token")
+      config.headers = {
+        'Authorization': `Bearer ${accessToken}`,
+        'Accept': 'application/json',
+      }
+      return config;
+    },
+    err => {
+      Promise.reject(err);
+    }
+  )
   return (
     <BrowserRouter>
-    {Modalstatus ? 
-      <LoginModal/>
-      : ''
-    }
-    {signup ? 
+      {Modalstatus ?
+        <LoginModal />
+        : ''
+      }
+      {signup ?
         ''
-    : <Header/>}
-    <Switch>
-        <Route exact path="/" component={MainPage}/>
-        <Route path="/upload" component={Upload}/>
-        <Route path="/profile" component={Profile}/>
-        <Route path="/signup" component={SignUp}/>
-        <Route path="/product" component={GoodsDetail}/>
+        : <Header />}
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <Route path="/upload" component={Upload} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/product" component={GoodsDetail} />
       </Switch>
     </BrowserRouter>
   );
