17f161e529af2723a795ccb5f31c2da61fdb8cb8
강석현 <tjrgus0713@gmail.com>
2021-11-17 22:58:44 +0900

    [추가] 검색창에서 검색이 가능하도록 추가

--- src/Components/Header/HeaderBottom/SearchBox.tsx
+++ src/Components/Header/HeaderBottom/SearchBox.tsx
@@ -4,20 +4,27 @@ import Symbol from '../../../assets/images/Symbol.png'
 import Logo09 from '../../../assets/images/logo_09.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 const SearchBox = () => {
-    return(
+    const history = useHistory()
+    const onSearch = (keyword: string) => {
+        history.push('/search?keyword=' + keyword)
+    }
+    const onKeyPressEnter = (e: any) => {
+        if (e.key === 'Enter' && e.target.value.length > 0) onSearch(e.target.value)
+    }
+    return (
         <S.HeaderLogoNSearch>
-            <Link to ="/">
+            <Link to="/">
                 <S.LogoNSymbol>
-                    <S.Logo img={Symbol} margin={11}/>
-                    <S.Logo isMain={true} img={Logo09}/>
+                    <S.Logo img={Symbol} margin={11} />
+                    <S.Logo isMain={true} img={Logo09} />
                 </S.LogoNSymbol>
             </Link>
             <S.SearchBox>
-                <input type="text" placeholder="상품명을 입력하세요." />
-                <FontAwesomeIcon style={{marginLeft:'10px'}} icon={faSearch}/>
+                <input type="text" placeholder="상품명을 입력하세요." onKeyPress={onKeyPressEnter} />
+                <FontAwesomeIcon style={{ marginLeft: '10px' }} icon={faSearch} />
             </S.SearchBox>
         </S.HeaderLogoNSearch>
     )
