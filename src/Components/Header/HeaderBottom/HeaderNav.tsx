3392b0405a7a710788b0bcdfe220d54150f407a1
강석현 <tjrgus0713@gmail.com>
2021-11-18 09:05:29 +0900

    [수정] 헤더 nav부분에 있는 링크 경로 수정

--- src/Components/Header/HeaderBottom/HeaderNav.tsx
+++ src/Components/Header/HeaderBottom/HeaderNav.tsx
@@ -1,33 +1,33 @@
 import React from 'react';
 import * as S from './styles';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlusSquare,faUserAlt,faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faPlusSquare, faUserAlt, faHeart } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom';
 const HeaderNav = () => {
-    return(
+    return (
         <S.NavWrapper>
             <Link to="/upload">
                 <S.NavA>
                     <li>
-                        <i><FontAwesomeIcon icon={faPlusSquare}/></i>
+                        <i><FontAwesomeIcon icon={faPlusSquare} /></i>
                         <p>게시물 올리기</p>
                     </li>
                 </S.NavA>
             </Link>
-            <S.Line/>
-            <Link to="/profile?type=my_page">
+            <S.Line />
+            <Link to="/profile?page=my_page&type=ing">
                 <S.NavA>
                     <li>
-                        <i><FontAwesomeIcon icon={faUserAlt}/></i>
+                        <i><FontAwesomeIcon icon={faUserAlt} /></i>
                         <p>마이페이지</p>
                     </li>
                 </S.NavA>
             </Link>
-            <S.Line/>
+            <S.Line />
             <Link to="/profile?type=wish">
                 <S.NavA>
                     <li>
-                        <i><FontAwesomeIcon icon={faHeart}/></i>
+                        <i><FontAwesomeIcon icon={faHeart} /></i>
                         <p>찜한 상품</p>
                     </li>
                 </S.NavA>
