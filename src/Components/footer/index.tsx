f5916282bf626cf1ce06a691f262fb74069595bc
강석현 <tjrgus0713@gmail.com>
2021-11-20 23:24:05 +0900

    [추기] 로그인상태가 false일 시 사용 못 하는 페이지에 들어가면 알림을 띄우며 메인페이지로 이동하도록 추가

--- src/Components/footer/index.tsx
+++ src/Components/footer/index.tsx
@@ -1,14 +1,26 @@
 import React, { useEffect } from 'react';
 import * as S from './styles'
-import { useDispatch } from 'react-redux';
+import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router';
 import { setSignuUp } from '../../modules/redux/action/page';
 import Developers from './developers';
 import Project09 from './project';
+import { RootState } from '../../modules/redux';
 function Footer() {
     const location = useLocation();
     const dispatch = useDispatch()
     const setPage = () => dispatch(setSignuUp(location.pathname))
+    const page = useSelector((state: RootState) => state.setPage.page);
+    const isLogin = useSelector((state: RootState) => state.setIsLogin.isLogin)
+    const history = useHistory()
+    console.log(page)
+    useEffect(() => {
+        if (isLogin === false && (page !== '' && page !== '/' && page !== '/search' && page !== '/product' && page !== '/profile')) {
+            history.push('/')
+            alert('로그인이 필요합니다!')
+        }
+    }, [page])
     useEffect(() => {
         setPage()
     }, [location.pathname])
