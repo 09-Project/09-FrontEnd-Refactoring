c3d69560f42214dd0fc8791efbd6a4b1874bfbe1
강석현 <tjrgus0713@gmail.com>
2021-11-22 10:45:43 +0900

    [상품 상세페이지에서 프로필로 이동시 그 프로필이 내 것이면 마이페이지처럼 뜨도록 수정

--- src/Components/Porfile/index.tsx
+++ src/Components/Porfile/index.tsx
@@ -5,6 +5,8 @@ import * as S from './styles';
 import UserActive from './UserActive';
 import UserInfo from './UserInfo';
 import { useLocation } from 'react-router';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../modules/redux';
 function useQuery() {
     return new URLSearchParams(useLocation().search);
 }
@@ -24,8 +26,12 @@ function Profile() {
     const type = query.get("type");
     const page = query.get('page')
     const id = query.get('id');
-    const [isMyPage, setIsMyPage] = useState(page === 'my_page');
     const [userInfo, setUserInfo] = useState<userInfoType>({})
+    const memberName = useSelector((state: RootState) => state.setMember.info.name)
+    const [isMyPage, setIsMyPage] = useState(memberName === userInfo.name || page === 'my_page');
+    console.log(isMyPage)
+    console.log(userInfo.name)
+    console.log(memberName)
     useEffect(() => {
         if (isMyPage) {
             axios.get(API_HOST + '/member/my-page', {
@@ -35,9 +41,15 @@ function Profile() {
                     setUserInfo(res.data)
                 })
         } else {
-            axios.get(`${API_HOST}/member/${id}`).then(res => setUserInfo(res.data))
+            axios.get(`${API_HOST}/member/${id}`).then(res => {
+                setUserInfo(res.data)
+            })
         }
     }, [])
+    useEffect(() => {
+        if (userInfo.name === memberName) setIsMyPage(true)
+        else setIsMyPage(false)
+    }, [userInfo.name])
     return (
         <S.ProfileWrapper>
             <S.Title>마이페이지</S.Title>
