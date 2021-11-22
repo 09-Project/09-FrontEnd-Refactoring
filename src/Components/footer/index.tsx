7a9a447a92b04a170c7e938addf8688f0e7a8468
강석현 <tjrgus0713@gmail.com>
2021-11-22 10:34:43 +0900

    [추가] 로그인 없이 접근 불가능한 페이지 추가

--- src/Components/footer/index.tsx
+++ src/Components/footer/index.tsx
@@ -7,16 +7,21 @@ import { setSignuUp } from '../../modules/redux/action/page';
 import Developers from './developers';
 import Project09 from './project';
 import { RootState } from '../../modules/redux';
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+}
 function Footer() {
+    const query = useQuery();
     const location = useLocation();
     const dispatch = useDispatch()
     const setPage = () => dispatch(setSignuUp(location.pathname))
+    console.log(location)
     const page = useSelector((state: RootState) => state.setPage.page);
     const isLogin = useSelector((state: RootState) => state.setIsLogin.isLogin)
     const history = useHistory()
-    console.log(page)
+    console.log((page !== '/profile' && query.get('page') !== 'my_page'))
     useEffect(() => {
-        if (isLogin === false && (page !== '' && page !== '/' && page !== '/search' && page !== '/product' && page !== '/signup')) {
+        if (isLogin === false && (page !== '' && page !== '/' && page !== '/search' && page !== '/product' && page !== '/signup' && (page === '/profile' && query.get('page') === 'my_page'))) {
             history.push('/')
             alert('로그인이 필요합니다!')
         }
